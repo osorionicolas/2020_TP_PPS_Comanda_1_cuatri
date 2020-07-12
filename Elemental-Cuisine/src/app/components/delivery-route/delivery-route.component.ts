0451f1bad572959242ca6f351eab59e09c39160e
Nicolas Osorio <osorionicolas95@gmail.com>
2020-07-11 23:10:49 -0300

    Ajustes delivery

--- Elemental-Cuisine/src/app/components/delivery-route/delivery-route.component.ts
+++ Elemental-Cuisine/src/app/components/delivery-route/delivery-route.component.ts
@@ -86,7 +86,8 @@ export class DeliveryRouteComponent implements OnInit, AfterViewInit {
   }
 
   geocode(latitude, longitude){
-    return this.http.get(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${latitude},${longitude}&key=AIzaSyA7j2g8L3aaLrjH8J7EGhA3-FHs_oZqE4w`);
+    const key = ""
+    return this.http.get(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${latitude},${longitude}&key=${key}`);
   }
 
   // Permite tener un buscado de direcciones correspondientes a Argentina
